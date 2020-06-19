d748e3e196560722e38c1121cfbb73272e12e3c6
Daniel Mello <dmmello1989@gmail.com>
2020-06-19 15:24:59 -0300

    Dashboard configuração inicial

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,49 +12,45 @@ import dinner from "../../assets/dinner.svg";
 
 const Home = () => {
   return (
-    <Fragment>
+    <div className="main">
       
       <TopMenu />
       
-      <div class="container">
-
-        <div class="main-center">
-          <div class="main-center-left">
-            <h1>Organize os convidados e as comidas da sua festa.</h1>
-            <h2>Confirme sua presença e registre sua contribuição.</h2>
+      <div className="main-content">
+        <div class="container">
+          <div class="main-center">
+            <div class="main-center-left">
+              <h1>Organize os convidados e as comidas da sua festa.</h1>
+              <h2>Confirme sua presença e registre sua contribuição.</h2>
+            </div>
+            
+            <div class="main-center-divider"></div>
+            
+            <div class="main-center-right">
+              <LoginForm />
+            </div>
           </div>
           
-          <div class="main-center-divider"></div>
-          
-          <div class="main-center-right">
-            <LoginForm />
+          <div class="main-bottom">
+            <HomeCard
+              cardImage={plan}
+              cardTitle="Planeje seu Evento"
+              cardText="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            />
+            <HomeCard
+              cardImage={guest}
+              cardTitle="Controle de convidados"
+              cardText="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            />
+            <HomeCard
+              cardImage={dinner}
+              cardTitle="Comida da festa"
+              cardText="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            />
           </div>
         </div>
-        
-        <div class="main-bottom">
-          <HomeCard
-            cardImage={plan}
-            cardTitle="Planeje seu Evento"
-            cardText="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-          />
-          <HomeCard
-            cardImage={guest}
-            cardTitle="Controle de convidados"
-            cardText="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-          />
-          <HomeCard
-            cardImage={dinner}
-            cardTitle="Comida da festa"
-            cardText="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-          />
-        </div>
-
-        <footer>
-
-        </footer>
-
       </div>
-    </Fragment>
+    </div>
   )
 }
 
